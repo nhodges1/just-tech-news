194c09430e804529d2f724b6019d1113cf8517b3
nhodges <hodges27@outlook.com>
2022-06-08 20:38:42 -0400

    homepage

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const routes = require('./controllers');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
