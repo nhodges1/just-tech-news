7beeedb06f1cca3032161d56aac898dcf3fc3567
nhodges <hodges27@outlook.com>
2022-06-09 03:42:40 -0400

    connectionjs

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+  
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
